d09c96a83d69a8bdd1fa97268079ea454361425a
AngelPlayer <ssh5212@naver.com>
2022-07-14 14:40:56 +0900

    commit

---
+++ test.js
@@ -0,0 +1,10 @@
+// const angelplayer = function(data1, data2) {
+//     return data3;
+// };
+
+
+// const angelplayer = (data1, data2) => {
+//     return data3;
+// };
+
+const angelplayer = (data1, data2) => data3;
